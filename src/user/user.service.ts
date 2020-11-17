4195efc1334ba47ffd436f68e92a0659c5c100c3
Haroldo <haroldobeyerbacellar@gmail.com>
2020-11-17 08:49:42 -0300

    Adding user creation

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotAcceptableException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
+
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+    constructor(@InjectRepository(User) private userRepository: Repository<User>) { }
+
+    async findOne(email: string): Promise<User | undefined> {
+        return this.userRepository.findOne({
+            where: [{ email }]
+        });
+    }
+
+    async createUser(password: string, email: string) {
+        const user = await this.findOne(email);
+
+        if (user)
+            throw new NotAcceptableException();
+
+        password = bcrypt.hashSync(password, 10);
+
+        const newUser = { email, password };
+        return await this.userRepository.insert(newUser);
+    }
+}
--- src/user/users.controller.ts
+++ src/user/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    constructor(private userService: UserService) { }
+
+    @Post()
+    async createUser(@Body() body) {
+        return this.userService.createUser(body.password, body.email);
+    }
+}
